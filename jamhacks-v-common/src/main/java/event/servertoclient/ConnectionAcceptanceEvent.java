e8e6a616c58d5394300c2577a9cf34a3241e4264
JaryJay <jay.jrjren@gmail.com>
2021-05-23 10:09:20 -0400

    Update ConnectionAcceptanceEvent.java

--- jamhacks-v-common/src/main/java/event/servertoclient/ConnectionAcceptanceEvent.java
+++ jamhacks-v-common/src/main/java/event/servertoclient/ConnectionAcceptanceEvent.java
@@ -1,7 +1,5 @@
 package event.servertoclient;
 
-import state.GameState;
-
 public class ConnectionAcceptanceEvent extends ServerToClientGameEvent {
 
 	/**
@@ -9,9 +7,6 @@ public class ConnectionAcceptanceEvent extends ServerToClientGameEvent {
 	 */
 	private static final long serialVersionUID = -8108701449003594930L;
 	private long userId;
-	private GameState state;
-	private long nextEventId;
-	private long nextActorId;
 
 	public long getUserId() {
 		return userId;
@@ -21,33 +16,9 @@ public class ConnectionAcceptanceEvent extends ServerToClientGameEvent {
 		this.userId = userId;
 	}
 
-	public GameState getState() {
-		return state;
-	}
-
-	public void setState(GameState state) {
-		this.state = state;
-	}
-
 	@Override
 	public String getDescription() {
 		return "Connection accepted";
-	}
-
-	public long getNextEventId() {
-		return nextEventId;
-	}
-
-	public void setNextEventId(long nextEventId) {
-		this.nextEventId = nextEventId;
-	}
-
-	public long getNextActorId() {
-		return nextActorId;
-	}
-
-	public void setNextActorId(long nextActorId) {
-		this.nextActorId = nextActorId;
 	}
 
 }
