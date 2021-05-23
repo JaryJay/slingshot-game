3e7e2d5d3081d8d8e99b735a038b4fc10406bff3
JaryJay <jay.jrjren@gmail.com>
2021-05-23 10:12:10 -0400

    Revert "Update ConnectionAcceptanceEvent.java"
    This reverts commit e8e6a616c58d5394300c2577a9cf34a3241e4264.

--- jamhacks-v-common/src/main/java/event/servertoclient/ConnectionAcceptanceEvent.java
+++ jamhacks-v-common/src/main/java/event/servertoclient/ConnectionAcceptanceEvent.java
@@ -1,5 +1,7 @@
 package event.servertoclient;
 
+import state.GameState;
+
 public class ConnectionAcceptanceEvent extends ServerToClientGameEvent {
 
 	/**
@@ -7,6 +9,9 @@ public class ConnectionAcceptanceEvent extends ServerToClientGameEvent {
 	 */
 	private static final long serialVersionUID = -8108701449003594930L;
 	private long userId;
+	private GameState state;
+	private long nextEventId;
+	private long nextActorId;
 
 	public long getUserId() {
 		return userId;
@@ -16,9 +21,33 @@ public class ConnectionAcceptanceEvent extends ServerToClientGameEvent {
 		this.userId = userId;
 	}
 
+	public GameState getState() {
+		return state;
+	}
+
+	public void setState(GameState state) {
+		this.state = state;
+	}
+
 	@Override
 	public String getDescription() {
 		return "Connection accepted";
+	}
+
+	public long getNextEventId() {
+		return nextEventId;
+	}
+
+	public void setNextEventId(long nextEventId) {
+		this.nextEventId = nextEventId;
+	}
+
+	public long getNextActorId() {
+		return nextActorId;
+	}
+
+	public void setNextActorId(long nextActorId) {
+		this.nextActorId = nextActorId;
 	}
 
 }
