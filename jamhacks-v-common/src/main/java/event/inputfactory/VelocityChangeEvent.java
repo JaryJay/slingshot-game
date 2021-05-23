78ffe05b5efa7c2b4ea6d685761606bccd161ce0
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-05-22 20:48:39 -0400

    Updated VelocityChangeEvent and added InputFrameEvent

--- jamhacks-v-common/src/main/java/event/inputfactory/VelocityChangeEvent.java
+++ jamhacks-v-common/src/main/java/event/inputfactory/VelocityChangeEvent.java
@@ -1,9 +1,14 @@
 package event.inputfactory;
 
+import event.clienttoserver.ClientToServerGameEvent;
 import math.Vector2f;
 
-public class VelocityChangeEvent {
+public class VelocityChangeEvent extends ClientToServerGameEvent {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4322031362243678381L;
 	Vector2f velocityVector;
 
 	public VelocityChangeEvent(float MoveX, float MoveY) {
