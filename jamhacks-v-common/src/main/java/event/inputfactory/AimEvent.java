af694666b55bbf9bdbf1a9282e9cf0a2b60ca086
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-05-22 23:19:51 -0400

    Updated AimEvent and ShootEvent

--- jamhacks-v-common/src/main/java/event/inputfactory/AimEvent.java
+++ jamhacks-v-common/src/main/java/event/inputfactory/AimEvent.java
@@ -1,5 +1,22 @@
 package event.inputfactory;
 
-public class AimEvent {
+import event.clienttoserver.ClientToServerGameEvent;
+import math.Vector2f;
+
+public class AimEvent extends ClientToServerGameEvent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6196191869943475614L;
+	public Vector2f aimVector;
+
+	public AimEvent(Vector2f aimVector) {
+		this.aimVector = aimVector;
+	}
+
+	public Vector2f getAimVector() {
+		return aimVector;
+	}
 
 }
