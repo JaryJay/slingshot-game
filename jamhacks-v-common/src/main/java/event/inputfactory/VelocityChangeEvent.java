2df79f284991ff0dd030c6ba090eb46d22eb5294
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-05-23 00:23:45 -0400

    Update VelocityChangeEvent.java

--- jamhacks-v-common/src/main/java/event/inputfactory/VelocityChangeEvent.java
+++ jamhacks-v-common/src/main/java/event/inputfactory/VelocityChangeEvent.java
@@ -9,11 +9,10 @@ public class VelocityChangeEvent extends ClientToServerGameEvent {
 	 * 
 	 */
 	private static final long serialVersionUID = -4322031362243678381L;
-	Vector2f velocityVector;
+	private Vector2f velocityVector;
 
-	public VelocityChangeEvent(float MoveX, float MoveY) {
-		velocityVector.x = MoveX;
-		velocityVector.y = MoveY;
+	public VelocityChangeEvent(float velocityX, float velocityY) {
+		velocityVector = new Vector2f(velocityX, velocityY);
 	}
 
 	public Vector2f GetMoveVector() {
